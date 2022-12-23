6ff38e2187d3dafae833f44733fda07dfae30d51
Evolve <busboys2007@gmail.com>
2022-12-23 17:23:03 +0100

    CompressEngine update2

---
+++ Compression/HuffmanTree.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compression
+{
+    public static class HuffmanTree
+    {
+        public static byte[] Huffman_Tree_Compress(byte[] file)
+        {
+            Console.WriteLine(file);
+            return new byte[0];
+        }
+    }
+}
