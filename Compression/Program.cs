1eddb0cca1120070bf7b260995c2622f3a195fe3
Evolve <busboys2007@gmail.com>
2022-12-23 01:23:13 +0100

    Přidejte soubory projektu.

---
+++ Compression/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compression
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] FileBytes = File.ReadAllBytes("test.txt");
+
+            for (int i = 0; i < FileBytes.Length; i++)
+            {
+                if (FileBytes[i] == Encoding.Default.GetBytes("O")[0])
+                {
+                    ConsoleColor InitColor = Console.ForegroundColor;
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.Write($"{FileBytes[i]} ");
+                    Console.ForegroundColor = InitColor;
+                }
+                else Console.Write($"{FileBytes[i]} ");
+
+
+                if (i == Console.WindowWidth)
+                {
+                    Console.Write('\n');
+                }
+            }
+
+            Console.ReadKey(true);
+        }
+    }
+}
